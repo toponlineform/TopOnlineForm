e3035c77ccf718abef3d31d5a487766cd07cd736
toponlineform <toponlineforms@gmail.com>
2025-12-05 02:31:40 +0530

    Create latestJobs.js

---
+++ src/data/latestJobs.js
@@ -0,0 +1,5 @@
+export const latestJobsData = [
+  // Yahan ID 1-99 wali jobs aayengi (Jaise SBI, HPRCA)
+  // Agar abhi data nahi hai to ise khali rakhne ke liye ye line uncomment karein:
+  // { id: 1, title: "Sample Job", category: "Latest Jobs", slug: "sample-job", postDate: "01/01/2025", links:[], importantDates:[], vacancyDetails:[] }
+];
