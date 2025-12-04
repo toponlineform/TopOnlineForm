de7d1e56864f2a189b79800729c3a08e6422866e
toponlineform <toponlineforms@gmail.com>
2025-12-05 01:18:49 +0530

    Update jobsData.js

--- src/jobsData.js
+++ src/jobsData.js
@@ -1,17 +1,14 @@
-// 1. Import all separate data files
-import { latestJobsData } from './data/latestJobs';
-import { admitCardData } from './data/admitCards';
-import { resultData } from './data/results';
-import { answerKeyData } from './data/answerKeys';
-import { syllabusData } from './data/syllabus';
-// import { previousPaperData } from './data/previousPapers'; // <--- ISKO COMMENT KAR DIYA
+// 1. Import section
+// import { previousPaperData } from './data/previousPapers'; // <--- ISKO COMMENT KAR DEIN ( // laga dein)
 
-// 2. Merge them into one single list
+// ...
+
+// 2. Merge section
 export const jobsData = [
   ...latestJobsData,
   ...admitCardData,
   ...resultData,
   ...answerKeyData,
   ...syllabusData,
-  // ...previousPaperData // <--- ISKO BHI COMMENT KAR DIYA
+  // ...previousPaperData  // <--- ISKO BHI COMMENT KAR DEIN
 ];
