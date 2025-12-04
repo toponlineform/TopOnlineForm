363b04985de72af8e3399a84fcccfa562c4178cd
toponlineform <toponlineforms@gmail.com>
2025-12-05 02:08:54 +0530

    Update jobsData.js

--- src/jobsData.js
+++ src/jobsData.js
@@ -1,3 +1,15 @@
-export const previousPaperData = [
-  // Future me yahan data dalenge
+import { latestJobsData } from './data/latestJobs';
+import { admitCardData } from './data/admitCards';
+import { resultData } from './data/results';
+import { answerKeyData } from './data/answerKeys';
+import { syllabusData } from './data/syllabus';
+import { previousPaperData } from './data/previousPapers';
+
+export const jobsData = [
+  ...latestJobsData,
+  ...admitCardData,
+  ...resultData,
+  ...answerKeyData,
+  ...syllabusData,
+  ...previousPaperData
 ];
