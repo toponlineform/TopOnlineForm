24d46dfb8b203bc8cee9d6d19a726cef92ebd663
toponlineform <toponlineforms@gmail.com>
2025-12-05 15:37:32 +0530

    Update jobsData.js

--- src/jobsData.js
+++ src/jobsData.js
@@ -1,14 +1,10 @@
-// 1. Import all separate data files
-// Note: Adding .js extension to ensure exact path matching
+import { latestJobsData } from './data/latestJobs';
+import { admitCardsData } from './data/admitCards';
+import { resultsData } from './data/results';
+import { answerKeysData } from './data/answerKeys';
+import { syllabusData } from './data/syllabus';
+import { previousPapersData } from './data/previousPapers';
 
-import { latestJobsData } from './data/latestJobs.js';       
-import { admitCardsData } from './data/admitCards.js';       
-import { resultsData } from './data/results.js';             
-import { answerKeysData } from './data/answerKeys.js';       
-import { syllabusData } from './data/syllabus.js';           
-import { previousPapersData } from './data/previousPapers.js'; 
-
-// 2. Merge them into one single list
 export const jobsData = [
   ...latestJobsData,
   ...admitCardsData,
