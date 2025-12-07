78ae08f7c6b134fca70c61d276b5d264733ea6eb
toponlineform <toponlineforms@gmail.com>
2025-12-07 19:52:08 +0530

    Update jobsData.js

--- src/jobsData.js
+++ src/jobsData.js
@@ -1,24 +1,15 @@
 import { newAnswerKeys } from './myAkey';
 import { latestJobs } from './myLjobs';
 import { admitCards } from './myAcards';
+import { results } from './myRslt';
 
 
   
-  // ==================================================
-  //              SYLLABUS (ID 4001 - 5000)
-  // ==================================================
-  // #region SYLLABUS (4001-5000)
   
-  // #endregion
-  // ==================================================
-  //              PREVIOUS PAPERS (ID 5001 - 6000)
-  // ==================================================
- // #region PREVIOUS PAPERS (5001-6000)
-  
-  // #endregion
 
 export const jobsData = [
   ...newAnswerKeys, // Naya Data
   ...latestJobs,
-  ...admitCards
+  ...admitCards,
+  ...results
 ];
