d7ecb473ca5fdfa49c3f109be8a0dbd23fee254d
toponlineform <toponlineforms@gmail.com>
2025-12-12 18:10:22 +0530

    Delete copy-404.js

--- copy-404.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-
-// Build hone ke baad index.html ko 404.html ke naam se copy kar do
-fs.copyFileSync('dist/index.html', 'dist/404.html');
-console.log('✅ Success: Copied index.html to 404.html for GitHub Pages');
